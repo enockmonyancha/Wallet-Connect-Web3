bf81f925cb4cc6e70673da436270ca55b581d980
enockmonyancha <monyanchaen@gmail.com>
2025-02-12 15:38:27 +0300

    Add new Bonk UI

--- pages/index.js
+++ pages/index.js
@@ -40,16 +40,20 @@ export default function Home() {
         />
       </div>
       {/* Header Text */}
-      <h1 className="text-3xl mt-5 sm:text-5xl font-bold text-center text-yellow-300">
-        ENDING THE YEAR WITH A BONK
+      <h1 className="text-2xl mt-5 sm:text-3xl font-bold text-center text-yellow-300">
+        BUILD YOUR BONKDRAGON
       </h1>
-      <h2 className="text-2xl sm:text-4xl mt-5 text-yellow-300">YEAR OF REWARDS</h2>
+      <h2 className="text-xl sm:text-3xl mt-5 text-yellow-300">YEAR OF REWARDS</h2>
+
+      <p className="mt-2 text-yellow-300">Year of the dragon has brought you fortune and prosperity.</p>
+      <p className="mt-2 text-yellow-300">Connect your wallet to check and claim!</p>
+
 
       <WalletBalance />
 
       {/* Dragon Section */}
-      <div className='mt-10'>
-          <img src="temple4.png" alt="Dragon" width="290" />
+      <div className='mt-5'>
+          <img src="temple4.png" alt="Dragon" width="210" />
         </div>
 
         <button onClick={() => open()} className="mt-5 px-4 sm:px-6 py-2 bg-yellow-300 text-red-600 font-bold rounded-lg shadow">
